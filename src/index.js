24429e0abd83059a2ec33365c3bf4077d80035d9
navarro571 <navarro571g@gmail.com>
2022-07-17 17:45:19 -0300

    creating theme file and LoginPage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import Router from './routes/router';
+import theme from './styles/theme';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
---
+++ src/styles/theme.js
@@ -0,0 +1,26 @@
+import { extendTheme, theme } from '@chakra-ui/react';
+export default extendTheme({
+  styles: {
+    global: {
+      body: {
+        backgroundColor: 'c3',
+      },
+    },
+  },
+  colors: {
+    c1: "#E7F6F2",
+    c2: "#A5C9CA",
+    c3: "#395B64",
+    c4: "#2C3333",
+    btn_success: "#438f54",
+    btn_danger: "#dc3545",
+    btn_warning: "#ffc107",
+    btn_info: "#17a2b8"
+  },
+  sizes: {
+    container: {
+    },
+  },
+  components: {
+  },
+});
