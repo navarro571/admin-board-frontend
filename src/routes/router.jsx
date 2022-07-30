d9bafc5e0c1fd1dc7a1dc0a36a559be71cc72dc6
edpe2003 <edpe2003@gmail.com>
2022-07-30 19:40:42 -0300

    Finish register page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 
 function Router() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
             <Route path='/home' element={<Home />} />
         </Routes>
     </BrowserRouter>
