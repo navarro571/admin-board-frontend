f097f9501ec54d2b335fa281d06beae2bf491ed1
navarro571 <navarro571g@gmail.com>
2022-09-06 00:13:24 -0300

    Creating User Table in UsersPage

--- src/subpages/HomeUsersPage.jsx
+++ src/subpages/HomeUsersPage.jsx
@@ -1,17 +1,20 @@
 import React, {useContext, useEffect, useState} from "react";
-import {Stack, Text} from "@chakra-ui/react";
+import {Stack, Table, TableCaption, TableContainer, Tbody, Td, Text, Tfoot, Th, Thead, Tr} from "@chakra-ui/react";
 import AppContext from "../contexts/AppContext";
 
 const HomeUsersPage = () => {
     const appContext = useContext(AppContext);
     const [users, setUsers] = useState();
+    const [headers, setHeaders] = useState();
     useEffect( () => {
         const res = getUsers();
         res.then((users) => {
             setUsers(users)
+            const user = users[0];
+            if (user) {
+                setHeaders(Object.keys(user));
+            }
         });
-
-
     }, []);
 
     const getUsers = async function () {
@@ -32,7 +35,27 @@ const HomeUsersPage = () => {
     }
     return (
         <Stack>
-            <Text>{users ? JSON.stringify(users) : ""}</Text>
+            <TableContainer>
+                <Table variant='simple'>
+                    <Thead>
+                        <Tr>
+                            {headers && headers.length ? headers.map(header => <Th>{header}</Th>) : <></>}
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        {users && users.length ? users.map(user => (
+                            <Tr>
+                                {Object.values(user)?.length ? Object.values(user).map(value => <Td>{value}</Td>) : <></>}
+                            </Tr>
+                        )) : <></>}
+                    </Tbody>
+                    <Tfoot>
+                        <Tr>
+                            {headers && headers.length ? headers.map(header => <Th>{header}</Th>) : <></>}
+                        </Tr>
+                    </Tfoot>
+                </Table>
+            </TableContainer>
         </Stack>
     )
 }
